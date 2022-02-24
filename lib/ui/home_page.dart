8b112ac94839584beaa615b3201b7f7c56b453a5
Sebastian Giraldo <shgirald@bancolombia.com.co>
2022-02-24 00:03:48 -0500

    widgets alert, button

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_app/ui/series_page.dart';
+import 'package:netflix_app/ui/widgets/button.dart';
+
+import 'commons/constants/constants.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key, required this.title}) : super(key: key);
@@ -34,24 +38,132 @@ class _HomePageState extends State<HomePage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: Container(
-                      margin: EdgeInsets.only(top: 70),
-                      height: 50,
-                      child: 
-                        Text(
-                          'Welcome!',
-                          style: TextStyle(color: Colors.white, fontSize: 30, fontFamily: 'Gilroy'),textAlign: TextAlign.justify,
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Align(
+                        alignment: Alignment.topCenter,
+                        child: Container(
+                          margin: EdgeInsets.only(top: 70),
+                          height: 50,
+                          child: 
+                            Text(
+                              'Welcome!',
+                              style: TextStyle(color: Colors.white, fontSize: 30, fontFamily: 'Gilroy', fontWeight: FontWeight.bold),textAlign: TextAlign.justify,
+                            ),
                         ),
-                        // Text(
-                        //   '$_counter',
-                        //   style: Theme.of(context).textTheme.headline4,
-                        // ),
-                      
-                    ),
+                      ),
+                      SizedBox(
+                        height: 100,
+                      ),
+                      ButtonCustom(text: 'Sign up', color: Colors.yellow, func: (){
+                        print('Hola');
+                        // Navigator.push(context, MaterialPageRoute(builder: (context) => SeriesPage()));
+                      }),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      ButtonCustom(text: 'Log in', color: Colors.yellow, func: (){
+                      }),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Text(
+                          'Forgot password?',
+                          style: TextStyle(color: Colors.white, fontSize: 30, fontFamily: 'Gilroy', decoration: TextDecoration.underline),textAlign: TextAlign.justify,
+                        ),
+                    ],
                   ),
+                  
                 ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  void _onDisplayBottomSheetTiposDocumentos() async {
+    await showModalBottomSheet(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(25.0))),
+        context: context,
+        backgroundColor: Colors.white,
+        isDismissible: true,
+        builder: (context) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Center(
+                  child: Container(
+                height: 40,
+                padding: EdgeInsets.symmetric(vertical: 17.0),
+                child: Image.asset("assets/images/img_down.png"),
+              )),
+              Center(
+                  child: Text(
+                "Tipo de identificación",
+                style: TextStyle(
+                    color: Constants.primaryTextColor,
+                    fontFamily: 'Gilroy',
+                    fontSize: 16),
+              )),
+              //searchDireccion(),
+              Flexible(
+                child: Container(child: _buildBottomNavigationMenu()),
+              ),
+            ],
+          );
+        }).then((value) {
+      if (value != null) {
+        setState(() {
+          // tipo = value;
+          // tipoController.text = tipo.descripcion;
+          // identificationController.text = '';
+        });
+      }
+    });
+  }
+
+  Widget _buildBottomNavigationMenu() {
+    return Container(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: _widgetInput()
+    );
+  }
+
+  _widgetInput() {
+    return GestureDetector(
+      onTap: () {
+      },
+      child: Container(
+        margin: EdgeInsets.only(top: 18, left: 25, right: 25),
+        height: 50,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+                bottom: BorderSide(color: Constants.primaryColor))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Flexible(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  '',
+                  textScaleFactor: 1.0,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: Constants.primaryColor,
+                      fontFamily: 'Gilroy'),
+                ),
               ),
             ),
           ],
---
+++ lib/ui/series_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SeriesPage extends StatefulWidget {
+  SeriesPage({Key? key}) : super(key: key);
+
+  @override
+  State<SeriesPage> createState() => _SeriesPageState();
+}
+
+class _SeriesPageState extends State<SeriesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
