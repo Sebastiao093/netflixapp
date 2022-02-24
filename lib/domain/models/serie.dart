df18a71ae2ddb244e8167e552ce841f13f81e35d
Sebastian Giraldo <shgirald@bancolombia.com.co>
2022-02-23 21:20:16 -0500

    model and constants

---
+++ lib/domain/models/serie.dart
@@ -0,0 +1,13 @@
+class Serie {
+  String? title;
+  String? urlImage;
+  String? description;
+  String? imageName;
+
+  Serie({
+    this.title,
+    this.urlImage,
+    this.description,
+    this.imageName,
+  });
+}
---
+++ lib/ui/commons/constants/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Constants{
+
+  static const primaryTextColor = Color(0xFFFFFF);
+  static const secondaryTextColor = Color(0x8C8C8C);
+  static const primaryColor = Color(0x191919);
+  static const secondaryColor = Color(0xFFD233);
+
+  static const String _username = 'maria';
+  static const String _password = 'password';
+  static const String _username2 = 'pedro';
+  static const String _password2 = '123456';
+
+  static final String baseUrl = 'https://image.tmdb.org/t/p/';
+
+}
